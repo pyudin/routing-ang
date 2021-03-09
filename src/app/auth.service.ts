b4e7783d10c6deb56398c3f64ce89b2da7749508
Pavel Yudin <pasha.judin@gmail.com>
2021-03-09 14:00:54 +0200

    routes. guards. Auth

---
+++ src/app/auth.service.ts
@@ -0,0 +1,16 @@
+export class AuthService {
+  loggedIn = false;
+
+  isAuth() {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(this.loggedIn), 1000);
+    });
+    return promise;
+  }
+  login() {
+    this.loggedIn = true;
+  }
+  logout() {
+    this.loggedIn = false;
+  }
+}
